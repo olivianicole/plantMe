124c8885fdc8b1bc28d476a4c7ea805e4a8b5b12
olivianicole <onyoung@umich.edu>
2021-03-31 21:39:13 -0400

    listing model compelete

---
+++ app/models/categories.py
@@ -0,0 +1,9 @@
+from .db import db
+
+class Category(db.Model):
+    __tablename__ = "categories"
+
+    id = db.Column(db.Integer, primary_key=True)
+    category = db.Column(db.String(100), nullable=False)
+
+    
