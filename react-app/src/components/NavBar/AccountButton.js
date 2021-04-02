c7ea5aa2be966095909413c9075677ac0907a1a9
olivianicole <onyoung@umich.edu>
2021-04-02 16:56:14 -0400

    account button working and positioned

--- react-app/src/components/NavBar/AccountButton.js
+++ react-app/src/components/NavBar/AccountButton.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import LogoutButton from "../auth/LogoutButton";
-// import "./AccountButton.css";
+import "./AccountButton.css";
 
 function AccountButton ({ setAuthenticated }) {
     const [ showMenu, setShowMenu ] = useState(false);
