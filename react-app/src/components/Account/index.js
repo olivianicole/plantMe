6377be919df7d2c3e8cf069abcea07e90f96b5ee
olivianicole <onyoung@umich.edu>
2021-04-05 17:29:45 -0400

    completed conditional rendering on account page

--- react-app/src/components/Account/index.js
+++ react-app/src/components/Account/index.js
@@ -1,59 +1,78 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { authenticate } from "../../store/session";
 import { useDispatch, useSelector } from 'react-redux';
 import ShopForm from "./ShopForm";
 import "./Account.css";
+import shopReducer from "../../store/shop";
 
 
 const Account = () => {
   const dispatch = useDispatch();
   const user = useSelector((state) => state?.session?.user?.current_user);
+  const [showFavorites, setShowFavorites] = useState(false);
+  const [showShopForm, setShowShopForm] = useState(false);
+  const [showShopInfo, setShowShopInfo] = useState(false);
   
   useEffect(() => {
     if (!user) {
       dispatch(authenticate());
     }
+
   }, [user, dispatch])
 
   let option;
-  const openFavorites = () => {
+
+  if (showFavorites) {
+    option = (
+      <div>show favorites</div>
+    )
+  } else if (showShopForm) {
+    option = (
+      <div>show shop form</div>
+    )
+  } else if (showShopInfo) {
+    option = (
+      <div>show shop info</div>
+    )
+  } else {
     option = (
       <>
       </>
     )
-    return option;
-  };
-
-  const openShopInfo = (user) => {
-    if (user.shop) {
-      option = (
-        <>
-        </>
-      )
-    } else {
-      option = (
-        <>
-          <div className="option-container">
-            <ShopForm />
-          </div>
-        </>
-      )
-    }
-    return option;
   }
 
+  const openFavorites = () => {
+    setShowFavorites(true);
+    setShowShopInfo(false);
+    setShowShopForm(false);
+  }
+
+  const openShopInfo = () => {
+    if (user.shop){
+      setShowShopInfo(true);
+      setShowShopForm(false);
+      setShowFavorites(false)
+    } else {
+      setShowShopForm(true);
+      setShowFavorites(false);
+      setShowShopInfo(false);
+    }
+  }
   return (
     <>
       <div className="acount-page-container">
           <div className="account-first-name">{user?.first_name}</div>
           <div className="account-links">
-            <button onClick={openFavorites} className="fav account-link-container">
+            <button onClick={() => openFavorites()} className="fav account-link-container">
               <i className="fas fa-heart account-heart"></i>
             </button>
-            <button onClick={openShopInfo} className="store account-link-container">
+            <button onClick={() => openShopInfo()} className="store account-link-container">
               <i className="fas fa-store account-store"></i>
             </button>
-          </div>
+            </div>
+            <div className="account-page-lower-container"> 
+              {option}
+            </div>
       </div>
     </>
   );
