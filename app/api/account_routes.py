8b55f4c46281a03631470f53c1bae0d916942247
olivianicole <onyoung@umich.edu>
2021-04-05 12:32:39 -0400

    backend form setup

--- app/api/account_routes.py
+++ app/api/account_routes.py
@@ -1,5 +1,26 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, session, request
 from flask_login import login_required
-from app.models import User, Shop
+from app.models import db, User, Shop
+from app.forms import ShopForm
 
 account_routes = Blueprint('account', __name__)
+
+@account_routes.route('/open-shop', methods=["POST"])
+def open_shop():
+    form = ShopForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit(): 
+        new_shop = Shop(
+                        shop_logo=form.shop_logo.data,
+                        name=form.name.data,
+                        owner_id=form.owner_id.data,
+                        description=form.description.data,
+                        city=form.description.data,
+                        state=form.state.data,
+                        country=form.country.data,
+        )
+        db.session.add(new_shop)
+        db.session.commit()
+        return new_shop.to_dict()
+    else:
+        return {"errors": "invalid submission"}
