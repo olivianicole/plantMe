9f6d2c43f48ca84b2dee20f544fc9d55f5b492d8
olivianicole <onyoung@umich.edu>
2021-04-06 11:49:17 -0400

    welcome message and account link styled

--- react-app/src/components/Home/index.js
+++ react-app/src/components/Home/index.js
@@ -1,14 +1,16 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getListings } from "../../store/listings";
+import { NavLink } from "react-router-dom";
 import Listing from "../Listing";
+import "./Home.css";
 
 const Home = ({ authenticated }) => {
     const dispatch = useDispatch();
     const listings = useSelector((state) => {
         return state.listings.allListings?.all_listings;
     })
-    
+    const user = useSelector((state) => state?.session?.user?.current_user);
     
 
     useEffect(() => {
@@ -16,10 +18,41 @@ const Home = ({ authenticated }) => {
             dispatch(getListings());
         }
     }, [dispatch, listings])
+
+    let userInfo;
+
+    if (user.shop) {
+        userInfo = (
+            <div className="something">
+                <NavLink className="home-page-account-link" to="/account">
+                    <div className="home-page-logo-img">
+                        <img src={user.shop.shop_logo} alt="Your Shop's Logo"/>
+                    </div>
+                    <div>View Your Account Details </div>
+                </NavLink>
+            </div>
+        )
+    } else {
+        userInfo = (
+            <div>
+                <NavLink className="home-page-account-link" to="/account">
+                    <i class="fas fa-store"></i>
+                    <div className="home-page-account-link-text">Looking to Open a Shop?</div>
+                </NavLink>
+            </div>
+        )
+    }
     return (
         <>
-            <div>
-                {listings?.map((listing) => <Listing key={listing.id} listing={listing} /> )}
+            <div className="home-page-container">
+                <div className="home-page-welcome-container">
+                {`Welcome Back, ${user.first_name}!`}
+                
+                    {userInfo}
+                </div>
+                <div>
+                    {listings?.map((listing) => <Listing key={listing.id} listing={listing} /> )}
+                </div>
             </div>
         </>
     )
