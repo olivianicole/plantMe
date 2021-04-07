4bd488aa7a32abbbbfc37c89b660369a62bd677f
olivianicole <onyoung@umich.edu>
2021-04-06 20:01:10 -0400

    carousel functioning

--- react-app/src/components/ListingPage/index.js
+++ react-app/src/components/ListingPage/index.js
@@ -1,7 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { getCurrentListing } from "../../store/listings";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router";
+import SimpleCarousel from 'simple-react-carousel';
+
+
+import "./ListingPage.css"
 
 const ListingPage = () => {
     const dispatch = useDispatch();
@@ -13,8 +17,26 @@ const ListingPage = () => {
         if (!listing) dispatch(getCurrentListing(params.id));
     }, [listing, dispatch, params]);
 
+    const options = {
+        items: 1,
+        loop: true,
+        mouseDrag: true,
+        touchDrag: true,
+        nav: true,
+    }
+
+
     return (
         <>
+            <div className="listing-page-container">
+                <div className="listing-image-carousel">
+                    <SimpleCarousel className="my-slider">
+                        <div><img src={listing?.image_1} alt={listing?.description}/></div>
+                        <div><img src={listing?.image_2 || ""} alt={listing?.description}/></div>
+                        <div><img src={listing?.image_3 || ""} alt={listing?.description}/></div>
+                    </SimpleCarousel>
+                </div>
+            </div>
         </>
     )
 };
