dd617f708d05b0b871de7486d8d01ce545b5a642
olivianicole <onyoung@umich.edu>
2021-04-09 14:16:47 -0400

    cart styling

--- react-app/src/components/CartModal/Cart.js
+++ react-app/src/components/CartModal/Cart.js
@@ -6,19 +6,34 @@ const Cart = () => {
 
     const items = useSelector((state) => state.cart?.cart);
 
-    console.log(items)
+    const priceValue = (price) => {
+        let returnPrice;
+        let p = price?.toString().length
+        
+        if (p === 1 || p === 2) returnPrice = (`$ ${price}.00`)
+        if (p === 3) returnPrice = (`$ ${price}0`)
+        if (p >= 4) returnPrice = (`$ ${price}`)   
+        return returnPrice;
+    }
+
     if (items) {
         return (
             <div className="cart-container">
-                <div>In Your Cart</div>
+                <div className="cart-title-text">View Your Cart</div>
                 {items?.map((item) => (
-                    <div>
-                        <div>{item.name}</div>
-                        <img src={item.image_1}/>
+                    console.log(item),
+                    <div className="cart-item-container">
+                        <img src={item.image}/>
+                        <div className="cart-item-name">{item.name}</div>
+                        <div className="cart-item-right-container">
+                            <div className="cart-item-price">{priceValue(item.price)}</div>
+                            <div className="cart-item-quantity">Quantity: {item.quantity}</div>
+                        </div>
                     </div>
 
                 )
                 )}
+                <button></button>
             </div>
         )
     } else {
